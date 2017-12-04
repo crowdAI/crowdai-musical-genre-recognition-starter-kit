b5f1680af11716dde92c45b39de8c98357c59701
Michaël Defferrard <michael.defferrard@epfl.ch>
2017-12-04 18:21:02 +0100

    fma.py: down-sampled version from utils.py in mdeff/fma

---
+++ fma.py
@@ -0,0 +1,50 @@
+import os
+import ast
+
+import pandas as pd
+
+
+def load(filepath):
+
+    filename = os.path.basename(filepath)
+
+    if 'features' in filename:
+        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+
+    if 'echonest' in filename:
+        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+
+    if 'genres' in filename:
+        return pd.read_csv(filepath, index_col=0)
+
+    if 'tracks' in filename:
+        tracks = pd.read_csv(filepath, index_col=0, header=[0, 1])
+
+        COLUMNS = [('track', 'tags'), ('album', 'tags'), ('artist', 'tags'),
+                   ('track', 'genres'), ('track', 'genres_all')]
+        for column in COLUMNS:
+            tracks[column] = tracks[column].map(ast.literal_eval)
+
+        COLUMNS = [('track', 'date_created'), ('track', 'date_recorded'),
+                   ('album', 'date_created'), ('album', 'date_released'),
+                   ('artist', 'date_created'), ('artist', 'active_year_begin'),
+                   ('artist', 'active_year_end')]
+        for column in COLUMNS:
+            tracks[column] = pd.to_datetime(tracks[column])
+
+        SUBSETS = ('small', 'medium', 'large')
+        tracks['set', 'subset'] = tracks['set', 'subset'].astype(
+                'category', categories=SUBSETS, ordered=True)
+
+        COLUMNS = [('track', 'genre_top'), ('track', 'license'),
+                   ('album', 'type'), ('album', 'information'),
+                   ('artist', 'bio')]
+        for column in COLUMNS:
+            tracks[column] = tracks[column].astype('category')
+
+        return tracks
+
+
+def get_audio_path(track_id):
+    tid_str = '{:06d}'.format(track_id)
+    return os.path.join('data', 'fma_medium', tid_str[:3], tid_str + '.mp3')
