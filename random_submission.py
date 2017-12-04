dd11a2f16604d62ef4f279b6b6b51f751948d174
Michaël Defferrard <michael.defferrard@epfl.ch>
2017-12-04 18:16:18 +0100

    update random submission example

--- random_submission.py
+++ random_submission.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python
-import crowdai
-import pandas as pd
-import numpy as np
+
 import glob
-import tempfile
+
 import argparse
+import numpy as np
+import crowdai
+
 parser = argparse.ArgumentParser(description='Submit Random Predictions')
 parser.add_argument('--api_key', dest='api_key', action='store', required=True)
 args = parser.parse_args()
 
 API_KEY = args.api_key
 
-CLASSES = ['Blues', 'Classical', 'Country', 'Easy Listening', 'Electronic', 'Experimental', 'Folk', 'Hip-Hop', 'Instrumental', 'International', 'Jazz', 'Old-Time / Historic', 'Pop', 'Rock', 'Soul-RnB', 'Spoken']
+CLASSES = ['Blues', 'Classical', 'Country', 'Easy Listening', 'Electronic',
+           'Experimental', 'Folk', 'Hip-Hop', 'Instrumental', 'International',
+           'Jazz', 'Old-Time / Historic', 'Pop', 'Rock', 'Soul-RnB', 'Spoken']
 HEADERS = ['file_id'] + CLASSES
 
 f = open("random_submission.csv", "w")
 
 def render_line(arr):
-	arr = [repr(x) for x in arr]
-	f.write(",".join(arr)+"\n")
+    arr = [repr(x) for x in arr]
+    f.write(",".join(arr)+"\n")
 
 render_line(HEADERS)
 
 TEST_FILES = sorted(glob.glob("data/crowdai_fma_test/*.mp3"))
+
 if len(TEST_FILES) == 0:
-	raise Exception("Unable to find the test files at : \
-					`data/crowdai_fma_test/*.mp3`.\n \
-					 Are you sure you downloaded the test set and \
-					 placed it at the right location ? ")
+    raise Exception("Unable to find the test files at: "
+                    "`data/crowdai_fma_test/*.mp3`.\n"
+                    "Are you sure you downloaded the test set and "
+                    "placed it at the right location ? ")
 
 for _file in TEST_FILES:
-  """
-  NOTE: This expects that you have already downloaded the test set
-  and it is available inside the data folder
-  """
-  _track_id = _file.split("/")[-1].replace(".mp3","")
-  predictions = np.random.rand((len(CLASSES))).tolist()
-
-  render_line([_track_id] + predictions)
+    # NOTE: This expects that you have already downloaded the test set
+    # and it is available inside the data folder.
+    _track_id = _file.split("/")[-1].replace(".mp3", "")
+    predictions = np.random.rand((len(CLASSES))).tolist()
+    render_line([_track_id] + predictions)
 
 f.close()
 
